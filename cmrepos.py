414c4a94a05dc223a68548b72e43467a07d1767f
CodeVij <b.vijithreddy@gmail.com>
2015-06-24 12:41:08 -0700

    Create cmrepos.py

---
+++ cmrepos.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+def main():
+	os.chdir('/etc/yum.repos.d')
+	removecm=('sudo rm cloudera-manager.repo')
+	os.system(removecm)
+	wgetcm=('sudo wget http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo')
+	os.system(wgetcm)
+
+if __name__ == "__main__":
+    main()
