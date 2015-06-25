7bb02f2c0a4546d951fe47879814a61479218328
CodeVij <b.vijithreddy@gmail.com>
2015-06-25 09:23:35 -0700

    Update cmrepos.py

--- cmrepos.py
+++ cmrepos.py
@@ -5,8 +5,9 @@ import os
 
 def main():
 	os.chdir('/etc/yum.repos.d')
-	removecm=('sudo rm cloudera-manager.repo')
-	os.system(removecm)
+	if os.path.isfile('cloudera-manager.repo'):
+		removecm=('sudo rm cloudera-manager.repo')
+		os.system(removecm)
 	wgetcm=('sudo wget http://archive.cloudera.com/cm5/redhat/6/x86_64/cm/cloudera-manager.repo')
 	os.system(wgetcm)
 
